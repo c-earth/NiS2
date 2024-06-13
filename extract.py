82bc38f1a3cf5add1a13368b94b44e8b32c1db0c
Abhijatmedhi Chotrattanapituk <c_earth@mit.edu>
2024-06-13 14:35:52 -0400

    extract

---
+++ extract.py
@@ -0,0 +1,42 @@
+import os
+import pandas as pd
+import numpy as np
+import pickle as pkl
+
+clean_folder_path = './data/2_clean'
+data_folder = 'fine'
+data_folder_path = os.path.join(clean_folder_path, data_folder)
+
+extracted_folder_path = './data/3_extracted'
+save_name = data_folder + '.pkl'
+extracted_file_path = os.path.join(extracted_folder_path, data_folder)
+
+def float_eval(string):
+    try:
+        return float(string)
+    except:
+        return string
+
+data = dict()
+for file in os.listdir(data_folder_path):
+    file_path = os.path.join(data_folder_path, file)
+    if file.endswith('.dat'):
+        with open(file_path) as f:
+            lines = []
+            for line in f.readlines():
+                lines.append(list(map(float_eval, list(map(str.strip, line.split(','))))))
+        
+        keys = [key.split()[0] for key in lines[0]]
+        valuess = lines[1:]
+        datum = [dict(zip(keys, values)) for values in valuess]
+
+        datum = pd.DataFrame(datum)
+
+        B = int(np.mean(datum['Field'])/10000)
+        data[B] = datum
+    elif file.endswith('.xlsx'):
+        datum = pd.read_excel(file_path)
+        B = file[5:-(len(data_folder) + 7)]
+    
+with open(extracted_file_path, 'wb') as f:
+    pkl.dump(data, f)
