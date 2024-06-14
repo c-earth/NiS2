5ca549b63e22489d3705b6dc88406b148ba30e5e
Abhijatmedhi Chotrattanapituk <c_earth@mit.edu>
2024-06-14 16:51:11 -0400

    background

--- fitting_functions.py
+++ fitting_functions.py
@@ -1,16 +1,5 @@
-import numpy as np
 from scipy.special import exprel
 import scipy.integrate as integrate
-
-def gaussian(x, x0, sigma):
-    return np.exp(-((x - x0) / sigma)**2 / 2) / sigma / np.sqrt(2 * np.pi)
-
-def transition(x, al, ar, bl, br, xc):
-    return (x-xc < 0)*(np.abs(x-xc) ** -al + bl) + (x-xc > 0)*(np.abs(x-xc) ** -ar + br)
-
-def uncertain_transition(x, al, ar, bl, br, xc, sigma):
-    return integrate.quad(lambda y: gaussian(x, y, sigma)*transition(y, al, ar, bl, br, xc), 0, np.inf)[0] +\
-           integrate.quad(lambda y: gaussian(x, y, sigma)*transition(y, al, ar, bl, br, xc), -np.inf, 0)[0]
 
 def Debye(x, xd):
     eta = xd/x
@@ -20,3 +9,13 @@ def Einstein(x, xe):
     eta = xe/x
     return 1 / (exprel(eta) * exprel(-eta))
 
+def Electron(x, gamma):
+    return gamma * x
+
+def phonon_electron(x, Ad, xd, Ae, xe, gamma):
+    return Ad * Debye(x, xd) + Ae * Einstein(x, xe) + Electron(x, gamma)
+
+def residuals(ps, x, y, func, cutoff = 0, penalty = 1, weight = 1):
+    res = y - func(x, *ps)
+    penalize = (res < 0) * (x < cutoff)
+    return weight * res * (1 + (penalty - 1) * penalize)
